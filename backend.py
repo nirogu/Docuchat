603f808aea302e10cbff7144b5008fe879c439f3
nirogu <code@nicolasrojas.me>
2024-09-13 12:33:03 -0500

    Make RAG accessible with API

--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
 """
-Loka Senior ML Engineer Tech Assessment.
+RAG pipeline accessible through RestAPI.
 
 Author
 ------
@@ -9,6 +9,7 @@ Nicolas Rojas
 # import libraries
 import os.path
 import yaml
+from fastapi import FastAPI
 from llama_index.core import (
     StorageContext,
     load_index_from_storage,
@@ -17,6 +18,7 @@ from llama_index.core import (
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.llms.ollama import Ollama
+from database_handler import create_index
 
 # load configuration variables
 with open("config.yaml", "r", encoding="utf8") as yfile:
@@ -34,7 +36,9 @@ Settings.chunk_size = chunk_size
 Settings.embed_model = HuggingFaceEmbedding(model_name=embedding_model)
 Settings.llm = Ollama(model=ollama_model, request_timeout=360.0)
 
-# check if stored index already exists
+# initiate FastAPI app
+app = FastAPI()
+
 if not os.path.exists(index_dir):
     # check if stored index already exists
     create_index(chroma_collection, documents_dir)
@@ -43,14 +47,33 @@ else:
     storage_context = StorageContext.from_defaults(persist_dir=index_dir)
     index = load_index_from_storage(storage_context)
 
-# query the index
+# define the index
 query_engine = index.as_query_engine()
-response = query_engine.query("What is SageMaker?")
 
-# print the response
-print(response)
 
-# print source information
-for source_node in response.source_nodes:
-    print(f"Source file: {source_node.node.metadata['file_name']}")
-    print(f"Content: {source_node.node.get_content()}")
+@app.post("/query/")
+def retrieve(query: str) -> dict:
+    """Run a query with the RAG pipeline.
+
+    Parameters
+    ----------
+    query : str
+        Question asked by the user, as a string.
+
+    Returns
+    -------
+    dict
+        Dictionary containing the answer given by the LLM and the relevant
+        documents.
+    """
+    global query_engine
+    response = query_engine.query(query)
+    result = {"response": response}
+
+    source_files = []
+    for source_node in response.source_nodes:
+        source_files.append(source_node.node.metadata["file_name"])
+    source_files = list(set(source_files))
+    result["source_files"] = source files
+
+    return result
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 pip install -r requirements.txt
 curl -fsSL https://ollama.com/install.sh | sh
 ollama pull tinyllama
+python database_handler.py -c
